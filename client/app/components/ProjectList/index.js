450778e383cad5b9167c6b8e4929694deb4cf6d8
Christoph Schulze <c.schulze@freenet.de>
2017-06-21 14:50:43 +0200

    added ProjectList component

---
+++ client/app/components/ProjectList/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* ProjectList
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class ProjectList extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+ProjectList.propTypes = {
+
+};
+
+export default ProjectList;
