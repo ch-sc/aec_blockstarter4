72dad9e00df2436e2d42b373bfd8d5310ad6b40d
Christoph Schulze <c.schulze@freenet.de>
2017-06-21 14:04:28 +0200

    initial project set up

---
+++ cli/cli.js
@@ -0,0 +1,138 @@
+const fs = require('fs')
+const commander = require('commander')
+const Web3 = require('web3')
+const TruffleContract = require('truffle-contract');
+  
+const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+const web3 = new Web3(provider);
+
+const account = web3.eth.accounts[0];
+
+const projectData = JSON.parse(fs.readFileSync('../dapp/build/contracts/Project.json'))
+const ProjectContract = new TruffleContract(projectData)
+ProjectContract.setProvider(provider)
+
+function createNewProject(title, description, projectStart, projectEnd, fundingGoal, fundingStart, fundingEnd) {
+  let address
+  return ProjectContract.new({
+    from: account,
+    gas: 4712388,
+    gasPrice: 100000000000
+  }).then(instance => {
+    address = instance.address
+    return instance.set(title, description, projectStart, projectEnd, fundingGoal, fundingStart, fundingEnd, {
+      from: account,
+      gas: 4712388,
+      gasPrice: 100000000000
+    })
+  }).then(result => {
+    console.log(address)
+  }).catch(err => console.error(err))
+}
+
+function getFundingStatus(address) {
+  return ProjectContract.at(address).then(instance => {
+    return instance.getFundingStatus();
+  }).then(result => {
+    console.log(result[0])
+    return result
+  }).catch(err => {
+    console.error(err)
+  })
+}
+
+function getProjectInfo(address) {
+  return ProjectContract.at(address).then(instance => {
+    return instance.getProjectInfo();
+  }).then(result => {
+    let i = 0
+    console.log('creator:', result[i++])
+    console.log('title:', result[i++].toString())
+    console.log('description:', result[i++].toString())
+    console.log('fundingAmount:', result[i++].toNumber())
+    console.log('fundingGoal:', result[i++].toNumber())
+    console.log('fundingStart:', result[i++].toNumber())
+    console.log('fundingEnd:', result[i++].toNumber())
+    console.log('balance:', result[i++].toNumber())
+    return result
+  }).catch(err => {
+    console.error(err)
+  })
+}
+
+function randomFund(address) {
+  const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  const account = web3.eth.accounts[getRandomInt(0, 9)]
+  const balanceBefore = web3.eth.getBalance(account).toNumber()
+  return ProjectContract.at(address).then(instance => {
+    return instance.fund({
+      from: account,
+      gas: 4712388,
+      gasPrice: 100000000000,
+      value: getRandomInt(10000, 20000)
+    });
+  }).then(result => {
+    const balanceAfter = web3.eth.getBalance(account).toNumber()
+    console.log('randomFund:', balanceBefore - balanceAfter)
+    return result
+  }).catch(err => console.error(err))
+}
+
+function remove(address) {
+  return ProjectContract.at(address).then(instance => {
+    return instance.remove({
+      from: account,
+      gas: 4712388,
+      gasPrice: 100000000000
+    });
+  }).then(result => {
+    console.log('project %s removed', address)
+    return result
+  }).catch(err => console.error(err))
+}
+
+//  node cli.js create "Test title" "My description" 1000000 "2017-06-15T15:00:00"
+commander.command('create')
+  .description('adds a new project')
+  .arguments('[title] [description] [fundingGoal] [fundingEnd]')
+  .action((title, description, fundingGoal, fundingEnd) => {
+    createNewProject(title, description, fundingGoal, Date.parse(fundingEnd))
+  })
+
+//  node cli.js status 0x....0
+commander.command('status')
+  .description('get the funding status of a project')
+  .arguments('[address]')
+  .action(address => {
+    getFundingStatus(address)
+  })
+  
+//  node cli.js info 0x....0
+commander.command('info')
+  .description('debug a project')
+  .arguments('[address]')
+  .action(address => {
+    getProjectInfo(address)
+  })
+  
+//  node cli.js fund 0x....0
+commander.command('fund')
+  .description('fund a project randomly')
+  .arguments('[address]')
+  .action(address => {
+    for (let i = 0; i < 5; i++) {
+      randomFund(address)
+    }
+  })
+  
+//  node cli.js remove 0x....0
+commander.command('remove')
+  .description('remove a project')
+  .arguments('[address]')
+  .action(address => {
+    remove(address)
+  })
+
+commander.parse(process.argv)
---
+++ dapp/contracts/Project.sol
@@ -0,0 +1,99 @@
+pragma solidity ^0.4.11;
+
+contract Project {
+	
+	struct Funding {
+		address backer;
+		uint date;
+		uint fundingAmount;
+	}
+
+	// the creator's address
+	address creator;
+	string public title;
+	string public description;
+	uint public fundingAmount; // in wei
+	uint fundingGoal = 0;
+	uint fundingStart; // timestamp
+	uint fundingEnd = 0; // timestamp
+
+	Funding[] fundings;
+
+	function Project() public {
+		// linking the creator to this project
+		creator = msg.sender;
+	
+	}
+	
+	function set(string newTitle, string newDescription, uint newFundingGoal, uint newFundingEnd) public {
+		title = newTitle;
+		description = newDescription;
+		fundingStart = now;
+		fundingGoal = newFundingGoal;
+		fundingEnd = newFundingEnd;
+	}
+
+	function remove() public payable {
+		if (creator != msg.sender) {
+      throw;
+		}
+			
+		for (uint i = 0; i < fundings.length; i++) {
+			Funding funding = fundings[i];
+			
+			if (!funding.backer.send(funding.fundingAmount)){
+				revert();
+				throw;
+			}
+			
+			// decrease the actual funding amount of this project in case something odd happens
+			fundingAmount = fundingAmount - funding.fundingAmount;
+		}
+
+		// If the funding amount of this project is not yet 0, something unusual happend and the remove process should 
+		// not get finalized. As error handling purposes all state changes will be reverted.
+		if (fundingAmount != 0) {
+			revert();
+			throw;
+		}
+
+		selfdestruct(creator);
+	}
+
+	// The fallback function does not throw an exception, but will use the transfered Ether ('msg.value') to back this project.
+	function() payable {
+		fund();
+	 }
+
+	/*
+	 * Backing the project with the passed amount of Ether (wei). 'msg.value' will be ignored.
+	 */
+	function fund() public payable returns (uint) {
+		if (fundingEnd <= now) {
+			throw;
+		}
+
+		fundings.push(Funding(msg.sender, now, msg.value));
+		
+		fundingAmount = fundingAmount + msg.value;
+		return fundingAmount;
+	}
+
+	function getFundingStatus() public constant returns (string, uint, uint) {
+		var currentStatus = "Initialized";
+
+		if (fundingEnd > now && fundingAmount < fundingGoal){
+			currentStatus = "Ongoing";
+		} else if (fundingEnd > now && fundingAmount > fundingGoal){
+			currentStatus = "GoalReached";
+		} else {
+			currentStatus = "Closed";
+		}
+
+		return (currentStatus, fundings.length, fundingAmount);
+	}
+
+	function getProjectInfo() public constant returns (address, string, string, uint, uint, uint, uint, uint) {
+		return (creator, title, description, fundingAmount, fundingGoal, fundingStart, fundingEnd, this.balance);
+	}
+}
